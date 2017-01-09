229dd3b6bc0bb073d108551cf610eaef28d00954
Delly Fofie <delly.fofie@gmail.com>
2017-01-09 15:30:28 +0100

    new changes

--- public/javascripts/HausCtrl.js
+++ public/javascripts/HausCtrl.js
@@ -3,7 +3,7 @@ angular.module("haus", ['ui-notification', 'angular-loading-bar'])
 .config(function(NotificationProvider, $interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
     NotificationProvider.setOptions({
-        delay: 10000,
+        delay: 3000,
         startTop: 20,
         startRight: 10,
         verticalSpacing: 20,
@@ -13,20 +13,43 @@ angular.module("haus", ['ui-notification', 'angular-loading-bar'])
     });
 })
 
-.controller('HausCtrl', function($scope, $http, Notification, $timeout) {
+.run(function($rootScope) {
+    $rootScope.apiUrl = "http://localhost:5000";
+})
 
+.controller('HausCtrl', function($scope, $http, Notification, $timeout, $rootScope, $interval) {
+
+    getValue();
     $scope.an = true;
     /**
      ** Ask the py Via an httpCall the status of the lamp
      **/
-    $timeout(function() {
-        Notification.error({
-            message: 'Server unreachable',
-            title: 'error occurs'
+    $interval(getValue, 60000);
+
+    function getValue() {
+        $http({
+            method: 'GET',
+            url: $rootScope.apiUrl + "/status/photo"
+        }).then(function successCallback(response) {
+            var value = Number(response.data);
+            value = value < 5 ? 5 : value > 30 ? 30 : value;
+            $scope.lightValue = value;
+            $scope.style = {
+                "opacity": $scope.lightValue / 100
+            };
+            Notification.success({
+                message: 'Brightness status in the Room fetched: ' + $scope.lightValue,
+                title: 'Aktualisiert'
+            });
+        }, function errorCallback(response) {
+            Notification.error({
+                message: 'Server unreachable',
+                title: 'error occurs'
+            });
         });
-    }, 1200);
 
 
+    }
     $scope.getBrightnessStatus = function(apiUrl) {
         console.log(apiUrl);
     }
