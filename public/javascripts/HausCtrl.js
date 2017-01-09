2e523ddab778cc5d61a0c3897b42311e63845bf9
Delly Fofie <delly.fofie@gmail.com>
2017-01-09 13:29:39 +0100

    new changes

--- public/javascripts/HausCtrl.js
+++ public/javascripts/HausCtrl.js
@@ -1,24 +1,34 @@
-angular.module("haus", ['ui-notification'])
+angular.module("haus", ['ui-notification', 'angular-loading-bar'])
 
-.config(function(NotificationProvider) {
-        NotificationProvider.setOptions({
-            delay: 10000,
-            startTop: 20,
-            startRight: 10,
-            verticalSpacing: 20,
-            horizontalSpacing: 20,
-            positionX: 'left',
-            positionY: 'bottom'
-        });
-    })
+.config(function(NotificationProvider, $interpolateProvider) {
+    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    NotificationProvider.setOptions({
+        delay: 10000,
+        startTop: 20,
+        startRight: 10,
+        verticalSpacing: 20,
+        horizontalSpacing: 20,
+        positionX: 'left',
+        positionY: 'bottom'
+    });
+})
 
 .controller('HausCtrl', function($scope, $http, Notification, $timeout) {
 
+    $scope.an = true;
     /**
      ** Ask the py Via an httpCall the status of the lamp
      **/
-     $timeout(function() {
-       Notification.error({message: 'Server unreachable',  title: 'error occurs'});
-     }, 1200)   ;
+    $timeout(function() {
+        Notification.error({
+            message: 'Server unreachable',
+            title: 'error occurs'
+        });
+    }, 1200);
+
+
+    $scope.getBrightnessStatus = function(apiUrl) {
+        console.log(apiUrl);
+    }
 
 });
