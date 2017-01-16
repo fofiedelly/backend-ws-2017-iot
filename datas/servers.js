466b483da7afc86b3babd40dfd67635581ebd993
Delly Fofie <delly.fofie@gmail.com>
2017-01-16 01:08:05 +0100

    new changes on temperature display

--- datas/servers.js
+++ datas/servers.js
@@ -1,19 +1,19 @@
 var servers = [{
     "url": "/hsheilbronn-embeded-raum",
-    "apiurl": "http://wtf.wtf",
-    "name": "Room Server Hochschule Heilbronn Embedded raum",
+    "apiurl": "http://roomserver-ws2017.pagekite.me",
+    "name": "Room Server - Hochschule Heilbronn",
     "modules": [{
-        "name": "Licht remote seteurung",
-        "description": "lorem ipsum .......",
+        "name": "Licht Steurung",
+        "description": "Hier können Sie das Licht ein- und auschalten.",
         "slug": "light-remote-control"
     }, {
-        "name": "Heizugn remote seteurung",
-        "description": "lorem ipsum .......",
+        "name": "Heizung Steurung",
+        "description": "Hier können Sie die Heizung ein- und aufdrehen.",
         "slug": "temperature-remote-control"
-    }, ]
+    }]
 }, {
     "url": "/zimmer-fofie",
-    "apiurl": "http://wtf.wtf",
+    "apiurl": "http://roomserver-ws2017.pagekite.me",
     "name": "Zimmer Fofie @home",
     "modules": [{
         "name": "Licht remote seteurung",
--- public/javascripts/HausCtrl.js
+++ public/javascripts/HausCtrl.js
@@ -1,57 +1,83 @@
-angular.module("haus", ['ui-notification', 'angular-loading-bar'])
+angular.module("haus", ['ui-notification', 'angular-loading-bar', 'chart.js'])
 
-.config(function(NotificationProvider, $interpolateProvider) {
-    $interpolateProvider.startSymbol('[[').endSymbol(']]');
-    NotificationProvider.setOptions({
-        delay: 3000,
-        startTop: 20,
-        startRight: 10,
-        verticalSpacing: 20,
-        horizontalSpacing: 20,
-        positionX: 'left',
-        positionY: 'bottom'
-    });
-})
-
-.run(function($rootScope) {
-    $rootScope.apiUrl = "http://localhost:5000";
-})
-
-.controller('HausCtrl', function($scope, $http, Notification, $timeout, $rootScope, $interval) {
-
-    getValue();
-    $scope.an = true;
-    /**
-     ** Ask the py Via an httpCall the status of the lamp
-     **/
-    $interval(getValue, 60000);
-
-    function getValue() {
-        $http({
-            method: 'GET',
-            url: $rootScope.apiUrl + "/status/photo"
-        }).then(function successCallback(response) {
-            var value = Number(response.data);
-            value = value < 5 ? 5 : value > 30 ? 30 : value;
-            $scope.lightValue = value;
-            $scope.style = {
-                "opacity": $scope.lightValue / 100
-            };
-            Notification.success({
-                message: 'Brightness status in the Room fetched: ' + $scope.lightValue,
-                title: 'Aktualisiert'
-            });
-        }, function errorCallback(response) {
-            Notification.error({
-                message: 'Server unreachable',
-                title: 'error occurs'
-            });
+    .config(function(NotificationProvider, $interpolateProvider) {
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+        NotificationProvider.setOptions({
+            delay: 3000,
+            startTop: 20,
+            startRight: 10,
+            verticalSpacing: 20,
+            horizontalSpacing: 20,
+            positionX: 'left',
+            positionY: 'bottom'
         });
 
+        (function(ChartJsProvider) {
+            ChartJsProvider.setOptions({
+                colors: ['#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']
+            });
+        });
+    })
 
-    }
-    $scope.getBrightnessStatus = function(apiUrl) {
-        console.log(apiUrl);
-    }
+    .run(function($rootScope) {
+        $rootScope.apiUrl = "http://fofie.pagekite.me";
+    })
 
-});
+    .controller('HausCtrl', function($scope, $http, Notification, $timeout, $rootScope, $interval) {
+
+        getValue();
+        $scope.an = true;
+        /**
+         ** Ask the py Via an httpCall the status of the lamp
+         **/
+        $interval(getValue, 60000);
+
+        function getValue() {
+            $http({
+                method: 'GET',
+                url: $rootScope.apiUrl + "/status/photo"
+            }).then(function successCallback(response) {
+                var value = Number(response.data);
+                $scope.lightValue = value;
+                $scope.style = {
+                    "opacity": $scope.lightValue / 100
+                };
+                Notification.success({
+                    message: 'Brightness status in the Room fetched: ' + $scope.lightValue,
+                    title: 'Aktualisiert'
+                });
+            }, function errorCallback(response) {
+                Notification.error({
+                    message: 'Server unreachable',
+                    title: 'error occurs'
+                });
+            });
+
+
+        }
+        $scope.getBrightnessStatus = function(apiUrl) {
+            console.log(apiUrl);
+        };
+        $scope.data =   [9,-2,-2,9,9,19,20];
+        $scope.labels = ["01h", "03h", "06h", "08h", "12h", "14h", "16h", "19h", "21h"];
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }
+                ]
+            }
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+
+    });
